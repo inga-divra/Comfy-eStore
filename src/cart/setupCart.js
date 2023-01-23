86f546335a6835023a6f25fd7c8d4302ebc4d6c3
inga-divra <ingady@gmail.com>
2023-01-23 10:02:48 +0200

    addToCart setup

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -10,7 +10,30 @@ import { findProduct } from '../store.js';
 import addToCartDOM from './addToCartDOM.js';
 // set items
 
+const cartItemCountDOM = getElement('.cart-item-count');
+const cartItemsDOM = getElement('.cart-items');
+const cartTotalDOm = getElement('.cart-total');
+
+let cart = getStorageItem('cart')
+
 export const addToCart = (id) => {
-  console.log(id);
+  let item = cart.find((cartItem) => cartItem.id === id);
+  if (!item) {
+    let product = findProduct(id);
+    //add item to the cart
+    product = { ...product, amount: 1 };
+    cart = { ...cart, product };
+    //add item to the DOM
+    addToCartDOM(product)
+  } else {
+    //update values
+  }
+
+  // more stuff coming up
   openCart();
 };
+
+const init = () => {
+  console.log(cart);
+}
+init();
--- src/store.js
+++ src/store.js
@@ -9,6 +9,9 @@ const setupStore = (products) => {
     setStorageItem('store', store)
 };
 
-const findProduct = () => { };
+const findProduct = (id) => {
+    let product = store.find((product) => product.id === id)
+    return product
+};
 
 export { store, setupStore, findProduct };
